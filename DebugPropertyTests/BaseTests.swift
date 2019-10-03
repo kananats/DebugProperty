8162e6809ae6945166038521e980457a84e41eba
Kananat Suwanviwatana <kananat.s@gmail.com>
2019-10-03 22:15:00 +0900

    add comment

--- DebugPropertyTests/BaseTests.swift
+++ DebugPropertyTests/BaseTests.swift
@@ -1,3 +1,11 @@
+//
+//  BaseTests.swift
+//  DebugPropertyTests
+//
+//  Created by Kananat Suwanviwatana on 2019/10/03.
+//  Copyright © 2019 Kananat Suwanviwatana. All rights reserved.
+//
+
 import XCTest
 
 let key = "key"
